986db636ac6787132c025ed843efcf33c6cb0e6f
Nick Puckett <nick@puckettrand.com>
2025-10-01 08:34:33 -0400

    cow gif is working

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+let cowGif;
+
+function preload() 
+{
+	cowGif = loadImage('assets/cow.gif');
+}
+
+
 function setup() 
 {
 	createCanvas(400, 400);
@@ -5,5 +13,6 @@ function setup()
 
 function draw()
 {
-
+	background(220,200,200);
+	image(cowGif, 0, 0);
 }
